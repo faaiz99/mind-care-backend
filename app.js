cdde53644a811c086932c4edba57b767487e8cb5
Faaiz <faaizaslam75@live.com>
2023-05-22 19:09:46 +0500

    Patch added to Cors

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.get(`${baseUrl}/api-docs`, swaggerUi.setup(swaggerSpec));
 
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
   });
