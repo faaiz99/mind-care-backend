b9ea064288314a930ba6b59d44d21d9032bd4c0a
faaiz99 <44613754+faaiz99@users.noreply.github.com>
2023-08-31 14:26:59 +0500

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,4 @@ module.exports = {
   moduleDirectories: ["node_modules", "src"],
  // globalSetup:"./src/config/setup.config.ts",
   setupFilesAfterEnv: ["./src/config/setup.config.ts"],
-  collectCoverage: true,
-  coverageReporters: ["json", "html"],
 };
