e2cd8819f38191ed56ba091ecee1ad8052e26b6e
Faaiz <faaizaslam75@live.com>
2023-09-10 03:28:43 +0500

    Comment Service Completed

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,147 +1,77 @@
-import { Request, Response, NextFunction, RequestHandler} from 'express'
-import { Comment } from '../models/communityForums/comment.model.ts'
-import { Upvote } from '../models/communityForums/upvote.model.ts'
-import { Downvote } from '../models/communityForums/downvote.model.ts'
-import { Report } from '../models/communityForums/report.model.ts'
-import { Post } from '../models/communityForums/post.model.ts'
-import { uuid } from 'uuidv4';
+import { Request, Response, NextFunction, RequestHandler } from 'express'
+import * as commentService from '../services/comment.service.ts'
 
-export const createComment:RequestHandler = async (req:Request, res:Response, next:NextFunction) => {
-	const comment = req.body;
-	comment.commentId = uuid()
-	let result
+export const createComment: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		result = await Comment.create(Post)
+		const data = await commentService.createComment(req.body)
+		res.status(200).json({ status: 'success', message: 'Comment created', data })
 	} catch (error) {
-		//res.json({ status: 400, message: 'Comment could not be created', error: error.message })
 		next(error)
 	}
-	if (result != null || result != undefined) {
-		res.status(200).json({ status: 'success', message: 'Comment created', data: result })
-	}
-	next()
-
 }
 
-export const replyComment:RequestHandler = async (req:Request, res:Response, next:NextFunction) => {
-	let comment = req.body
-	comment.commentId = uuid()
-	let result;
+export const replyComment: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		comment = await Comment.create(req.body)
-		result = await Post.findOneAndUpdate({ commentId: req.params.id }, {
-			$push: { comments: comment }
-		})
+		const data = await commentService.replyComment(req.body, req.params.id)
+		res.status(200).json({ status: 'success', message: 'Comment Replied', data })
+
 	} catch (error) {
-		//res.json({ status: 400, message: 'Comment Replied Failed', error: error.message })
 		next(error)
 	}
-	if (result != null || result != undefined || comment != null || comment != undefined) {
-		res.status(200).json({ status: 'success', message: 'Comment Replied', data: result })
-	}
-	next()
-
 }
 
-export const updateComment:RequestHandler  = async (req:Request, res:Response, next:NextFunction) => {
-	const comment = req.body;
-	let result;
+export const updateComment: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		result = await comment.findOneAndUpdate({ commentId: req.params.id, comment })
+		const data = await commentService.updateComment(req.body, req.params.id)
+		res.status(200).json({ status: 'success', message: 'Comment updated', data })
 	} catch (error) {
-		//res.json({ status: 400, message: 'Comment could not be updated', error: error.message })
 		next(error)
 	}
-	if (result != null || result != undefined) {
-		res.status(200).json({ status: 'success', message: 'Comment updated', data: result })
-	}
-	next()
 }
 
-export const deleteComment:RequestHandler  = async (req:Request, res:Response, next:NextFunction) => {
-	// Delete Comment or Not ?/
-	const comment = req.body;
-	comment.body = 'Comment Deleted'
-	let result;
+export const deleteComment: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		result = await comment.findOneAndUpdate({ commentId: req.params.id, comment })
+		const data = await commentService.deleteComment(req.body, req.params.id)
+		res.status(200).json({ status: 'success', message: 'Comment updated', data })
 	} catch (error) {
-		//res.json({ status: 400, message: 'Comment could not be updated', error: error.message })
 		next(error)
 	}
-	if (result != null || result != undefined) {
-		res.status(200).json({ status: 'success', message: 'Comment updated', data: result })
-	}
-	next()
 }
 
-export const getComments:RequestHandler  = async (req:Request, res:Response, next:NextFunction) => {
-	let result;
+export const getComments: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		result = await Comment.find()
+		const data = await commentService.getComments()
+		res.status(200).json({ status: 'success', message: 'Comments retrieved', data })
+
 	} catch (error) {
-		//res.json({ status: 400, message: 'Comments could not be retrieved', error: error.message })
 		next(error)
 	}
-	if (result != null || result != undefined) {
-		res.status(200).json({ status: 'success', message: 'Comments retrieved', data: result })
-	}
-	next()
-
 }
 
-export const upvoteComment:RequestHandler  = async (req:Request, res:Response, next:NextFunction) => {
-	let upvote;
-	let result;
-
+export const upvoteComment: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		upvote = await Upvote.create(req.body)
-		result = await Comment.findOneAndUpdate({ commentId: req.params.id }, {
-			$push: { upvotes: upvote }
-		})
+		const data = await commentService.upvoteComment(req.body, req.params.id)
+		res.status(200).json({ status: 'success', message: 'Comment Upvoted', data })
 	} catch (error) {
-		//res.json({ status: 400, message: 'Comment could not be upvoted', error: error.message })
 		next(error)
 	}
-	if (result != null || result != undefined || upvote != null || upvote != undefined) {
-		res.status(200).json({ status: 'success', message: 'Comment upvoted', data: result })
-	}
-	next()
 }
 
-export const downvoteComment:RequestHandler  = async (req:Request, res:Response, next:NextFunction) => {
-	let downvote
-	let result;
+export const downvoteComment: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		downvote = await Downvote.create(req.body)
-		result = await Comment.findOneAndUpdate({ commentId: req.params.id }, {
-			$push: { downvotes: downvote }
-		})
+		const data = await commentService.downvoteComment(req.body, req.params.id)
+		res.status(200).json({ status: 'success', message: 'Comment Downvoted', data })
 	} catch (error) {
-		//res.json({ status: 400, message: 'Comment could not be downvoted', error: error.message })
 		next(error)
 	}
-	if (result != null || result != undefined || downvote != null || downvote != undefined) {
-		res.status(200).json({ status: 'success', message: 'Comment downvoted', data: result })
-	}
-	next()
 }
 
-export const reportComment:RequestHandler  = async (req:Request, res:Response, next:NextFunction) => {
-	let report;
-	let result;
+export const reportComment: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const data = await commentService.reportComment(req.body, req.params.id)
+		res.status(200).json({ status: 'success', message: 'Comment reported', data })
+	} catch (error) {
+		next(error)
+	}
 	
-	try {
-		report = await Report.create(req.body)
-		result = await Comment.findOneAndUpdate({ commentId: req.params.id }, {
-			$push: { postReport: report }
-		})
-	} catch (error) {
-		//res.json({ status: 400, message: 'Comment could not be reported', error: error.message })
-		next(error)
-	}
-	if (result != null || result != undefined || report != null || report != undefined) {
-		res.status(200).json({ status: 'success', message: 'Comment reported', data: result })
-	}
-	next()
 }
---
+++ src/services/comment.service.ts
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Comment } from '../models/communityForums/comment.model.ts'
+import { Upvote } from '../models/communityForums/upvote.model.ts'
+import { Downvote } from '../models/communityForums/downvote.model.ts'
+import { Report } from '../models/communityForums/report.model.ts'
+import { Post } from '../models/communityForums/post.model.ts'
+
+export const createComment = async (comment:any) =>{
+	const response = await Comment.create(comment)
+	if(!response)
+		throw new Error('Comment Could not be Created')
+	return response
+}
+
+export const replyComment = async (comment:any, id:string) =>{
+	const response = await Post.findOneAndUpdate({ _id: id }, {
+		$push: { comments: comment }
+	})
+	if(!response)
+		throw new Error('Reply Could not be Created')
+	return response
+	
+}
+
+export const updateComment = async (comment:any, id:string) =>{
+	const response = await Comment.findOneAndUpdate({_id:id}, comment)
+	if(!response)
+		throw new Error('Comment Updated')
+	return response
+}
+
+export const deleteComment = async (comment:any, id:string) =>{
+	comment.body = 'Comment Deleted'
+	const response = await Comment.findOneAndUpdate({_id:id}, comment)
+	if(!response)
+		throw new Error('Comment Deleted')
+	return response
+
+}
+
+export const getComments = async () =>{
+	const response = await Comment.find({})
+	if(!response)
+		throw new Error('Comments Could not be Retrieved')
+	return response
+}
+
+export const upvoteComment = async (comment:any, id:string) =>{
+	const upvote = await Upvote.create(comment)
+	const response = await Comment.findOneAndUpdate({_id:id},{
+		$push:{upvotes:upvote}
+	})
+	if(!response)
+		throw new Error('Comment Could not be Upvoted')
+	return response
+}
+
+export const downvoteComment = async (comment:any, id:string) =>{
+	const downvote = await Downvote.create(comment)
+	const response = await Comment.findOneAndUpdate({_id:id},{
+		$push:{downvotes:downvote}
+	})
+	if(!response)
+		throw new Error('Comment Could not be Downvoted')
+	return response
+}
+
+export const reportComment = async (comment:any, id:string) =>{
+	const report = await Report.create(comment)
+	const response = await Comment.findOneAndUpdate({ _id: id }, {
+		$push: { commentReport: report }
+	})
+	if(!response)
+		throw new Error('Comment Could not be Reported')
+	return response
+}
